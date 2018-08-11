afcf5f1f817c3e27cf52cd5290cc0152ceac9a3e
Arvanitis Christos <arvchristos@protonmail.com>
2018-08-11 22:10:11 +0300

    Repository refactor

--- install_script/installer.sh
+++ install_script/installer.sh
@@ -7,14 +7,13 @@ timestamp() {
   date +"%d%m%y_%H%M" #FORMAT DayMonthYear_HoursMinutes
 }
 
-
 WORKING_DIR="$(pwd)"
 UNOPKG_PATH="$(which unopkg)"
 LIBRECUST_MACRO_PATH="$(realpath ../)/menu_customization/macros/LibreCustLib.oxt"
 SETTINGS_LOADER_PATH="$(realpath ../)/settings_loader/settings_loader.oxt"
-PAGE_NUMBERING_PATH="$(realpath ../)/page_numbering_addon/Python\ version/PageNumberingAddonPython-0.0.1.oxt"
-AUTOTEXT_PATH="$(realpath ../)/autotext_addon/Sidebar_approach/Build_files/AutotextAddon-0.0.1.oxt"
-LAWADDON_PATH="$(realpath ../)/law_addon/Python\ Version/LawAddon-0.0.1.oxt"
+PAGE_NUMBERING_PATH="$(realpath ../)/page_numbering_addon/LibreOffice/python/build_files/PageNumberingAddonPython-0.0.1.oxt"
+AUTOTEXT_PATH="$(realpath ../)/autotext_addon/LibreOffice/sidebar_version/build_files/AutotextAddon-0.0.1.oxt"
+LAWADDON_PATH="$(realpath ../)/law_addon/LibreOffice/build_files/LawAddon-0.0.1.oxt"
 MENUBAR_PATH="$(realpath ../)/menu_customization/menubar/menubar.xml"
 TOOLBAR_PATH="$(realpath ../)/menu_customization/toolbar"
 USER_HOME_PATH="$(eval echo "~$different_user")"
@@ -42,6 +41,9 @@ fi
 
 killall soffice.bin
 
+#--------------------------------------------------------------------------------
+# Librecust macro library
+#--------------------------------------------------------------------------------
 read -p "Install macro library (Librecust.oxt)? [y/n] " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -52,6 +54,9 @@ fi
 
 eval $UNOPKG_PATH gui $LIBRECUST_MACRO_PATH 
 
+#--------------------------------------------------------------------------------
+# Settings Loader
+#--------------------------------------------------------------------------------
 read -p "Install Settings Loader (settings_loader.oxt)? [y/n] " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -62,6 +67,9 @@ fi
 
 eval $UNOPKG_PATH gui $SETTINGS_LOADER_PATH 
 
+#--------------------------------------------------------------------------------
+# Page Numbering
+#--------------------------------------------------------------------------------
 read -p "Install Page Numbering Addon (PageNumberingAddon.oxt)? [y/n] " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -70,7 +78,11 @@ then
     exit 1
 fi
 
-eval $UNOPKG_PATH gui $AUTOTEXT_PATH 
+eval $UNOPKG_PATH gui $PAGE_NUMBERING_PATH
+
+#--------------------------------------------------------------------------------
+# AutoText Addon
+#--------------------------------------------------------------------------------
 read -p "Install AutoText Addon (AutoTextAddon.oxt)? [y/n] " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -81,6 +93,9 @@ fi
 
 eval $UNOPKG_PATH gui $AUTOTEXT_PATH #unopkg gui because it is licensed and unopkg crashes when used with such extensions
 
+#--------------------------------------------------------------------------------
+# Law Addon
+#--------------------------------------------------------------------------------
 read -p "Install Law Toolbar (LawAddon.oxt)? [y/n] " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -91,6 +106,9 @@ fi
 
 eval $UNOPKG_PATH gui $LAWADDON_PATH #unopkg gui because it is licensed and unopkg crashes when used with such extensions
 
+#--------------------------------------------------------------------------------
+# Menu Bar
+#--------------------------------------------------------------------------------
 
 #Then we have to copy menubar after we backup previous menubar configuration with timestamp
 
@@ -111,6 +129,10 @@ cp $MENUBAR_PATH ./menubar.xml
 
 cd $WORKING_DIR #return to installer.sh path
 
+#--------------------------------------------------------------------------------
+# Toolbar
+#--------------------------------------------------------------------------------
+
 #Then we have to copy toolbars while backing up previous config
 read -p "Install Modified toolbars (toolbar/*.xml)? [y/n] " -n 1 -r
 echo    # (optional) move to a new line
@@ -128,6 +150,10 @@ cp ./*.xml "./toolbar_bak_$INSTALLATION_TIMESTAMP/"
 
 # Copy customized toolbars to User Profile
 cp "$TOOLBAR_PATH"/*.xml ./
+
+#--------------------------------------------------------------------------------
+# Icon theme
+#--------------------------------------------------------------------------------
 
 #Install icon theme (LibreOffice 5 needs the .zip file copied to install_dir. Version 6 allows bundling an icon theme in extension format)
 read -p "Install Office 2013 icon theme by charliecnr (toolbar/icon_theme/)? [y/n] " -n 1 -r
--- settings_loader/README.md
+++ settings_loader/README.md
@@ -4,3 +4,11 @@ This extension is a way to deploy certain configurations to LibreOffice. Such co
 Documentation regarding the structure of such extension can be found at the following sources:
 1. [Thorsten Behrens slides](http://www.linuxtag.org/2012/fileadmin/www.linuxtag.org/slides/Thorsten%20Behrens%20-%20LibreOffice%20configuration%20management%20-%20Tools_%20approaches%20and%20best%20practices.p331.pdf)
 2. [TDF wiki](https://wiki.documentfoundation.org/images/b/b0/LibreOffice_config_extension_writing.pdf)
+
+Following the previous guides, configure the files included in the extension directory. When ready, execute the following:
+
+```bash
+bash ./dir_to_oxt.sh 
+```
+
+The settings_loader.oxt file is the extension to be installed for configuration deployment.
